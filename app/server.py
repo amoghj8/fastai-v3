17ee43553cd20086b175059751a2fd5390237c1a
Amogh Joshi <amoghj8@gmail.com>
2019-03-21 15:28:46 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://s3.ap-south-1.amazonaws.com/dataset-fyp/fastAI/export.pkl'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
